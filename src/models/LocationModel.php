578708766817d36cc7148c26e75e05e3301aa13a
Lucas Weijers <lucas@dolphiq.nl>
2017-10-19 11:10:36 +0200

    add function to display a map per location

--- src/models/LocationModel.php
+++ src/models/LocationModel.php
@@ -4,6 +4,7 @@ namespace plugins\dolphiq\locationPicker\models;
 
 use Craft;
 use craft\base\Model;
+use plugins\dolphiq\locationPicker\assets\siteAsset;
 
 /**
  * Created by PhpStorm.
@@ -18,6 +19,8 @@ class LocationModel extends Model{
     public $long = "";
     public $address = "";
 
+    private $uniqueId;
+
     public function rules()
     {
         return [
@@ -30,5 +33,37 @@ class LocationModel extends Model{
         return [
             'address' => Craft::t('site', 'Address'),
         ];
+    }
+
+    public function getMap(){
+
+        // Load assets for map. Assetmanager will determine if they are already loaded
+        $this->registerAssets();
+
+        $uniqueId = $this->getUniqueId();
+        $json_locations = json_encode([$this->attributes]);
+
+        $script = <<<SCRIPT
+loadDolphiqMap('$uniqueId', $json_locations);
+SCRIPT;
+
+        Craft::$app->view->registerJs($script);
+
+
+        return '<div id="'.$uniqueId.'" class="dolphiqMap"></div>';
+    }
+
+    public function getUniqueId(){
+        if(empty($this->uniqueId)){
+            $this->uniqueId = 'dolphiq-craft3-locationpicker-'.uniqid().'-'.time();
+        }
+
+        return $this->uniqueId;
+    }
+
+    private function registerAssets(){
+        Craft::$app->view->registerAssetBundle(siteAsset::className());
+        Craft::$app->view->registerJsFile('https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js');
+        Craft::$app->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . \plugins\dolphiq\locationPicker\Plugin::getInstance()->getSettings()->apiKey . '&callback=initDolphiqMap', ['defer' => 'defer', 'async' => 'async']);
     }
 }
